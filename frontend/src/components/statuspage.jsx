d0c781ae53b874ccee89c4d07900d42c7d1b31a2
Bhuvaneswari <bhuvaneswari.3tech@gmail.com>
2026-07-04 11:20:52 +0530

    fixed css file names

--- frontend/src/components/statuspage.jsx
+++ frontend/src/components/statuspage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./statusPage.css";
+import "./statuspage.css";
 
 function StatusPage() {
   const navigate = useNavigate();
