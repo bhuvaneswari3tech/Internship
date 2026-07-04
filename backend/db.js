2aceae9ff5b51971e9ef89a02c23b8bcf32ac92a
Bhuvaneswari <bhuvaneswari.3tech@gmail.com>
2026-07-04 12:42:43 +0530

    db url updated in db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,19 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   ssl: {
+//         rejectUnauthorized: false,
+//     },
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: "postgresql://neondb_owner:npg_APg9Ml8kKJuY@ep-noisy-firefly-at1g8u1i-pooler.c-9.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require",
   ssl: {
         rejectUnauthorized: false,
     },
