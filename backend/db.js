b38bbcaa1e6c42013d61931e7cb4932695c73b00
Bhuvaneswari <bhuvaneswari.3tech@gmail.com>
2026-07-04 12:29:57 +0530

    db config corrected

--- backend/db.js
+++ backend/db.js
@@ -8,8 +8,8 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   ssl: {
-        rejectUnauthorized: false
-    }
+        rejectUnauthorized: false,
+    },
 });
 
 pool.connect()
